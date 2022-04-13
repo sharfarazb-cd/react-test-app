f254dc0137da45a39eda9f7fafe86729c124730d
Sharfaraz <sharfarazb@clouddestinations.com>
2022-04-13 22:23:02 +0530

    Basic app using class components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+
+import './App.css';
+import React from 'react';
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    console.log('Constructor');
+    super();
+    this.state = {
+     users:[],
+     searchField: ''
+    };
+  }
+
+  componentDidMount(){
+    console.log('componentDidMount');
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    .then((responseJson) => {
+      this.setState(
+        () => {
+          return { users: responseJson};
+        },
+        () => {
+          console.log(this.state);
+        }
+      )
+    }
+
+    );
+  }
+
+
+  render(){
+    console.log('render');
+    const filteredUsers = this.state.users.filter( (user) => {
+      return user.name.toLocaleLowerCase().includes(this.state.searchField);
+    })
+    const onChangeHandler = (event) => {
+      console.log(event)
+      const searchField = event.target.value.toLowerCase();
+      this.setState(() => {
+        return { searchField }
+      });
+    };
+
+    return (
+      <div className="App">
+        <h1 className='app-title'>Users</h1>
+        <SearchBox
+          className='search-box'
+          placeholder='Search Users'
+          onChangeHandler={onChangeHandler}
+        />
+        <CardList itemList={filteredUsers}/>
+      </div>
+    );
+  }
+
+  
+  
 }
 
 export default App;
